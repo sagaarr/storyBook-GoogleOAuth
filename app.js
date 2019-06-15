58f50e18d66eaf9141fc392db714fdce3ddd9f2c
sagar chinchorkar <sagarsc17@gmail.com>
2019-06-15 16:36:15 +0530

    auth implemented

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
 
+// passport config
+require('./config/passport')(passport);
 const app = express();
+// Load routes
+const auth = require('./routes/auth');
 
 app.get('/', (req,res) => {
   res.send('project started');
 })
+
+app.use('/auth', auth);
+
 
 const port = process.env.PORT || 3000
 app.listen(port , ()=>{
