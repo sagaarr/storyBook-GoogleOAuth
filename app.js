eb629090fd70d84d9c03139f947ce26de985c57d
sagar chinchorkar <sagarsc17@gmail.com>
2019-06-16 21:47:12 +0530

    story book google authentication added

--- app.js
+++ app.js
@@ -1,18 +1,56 @@
 const express = require('express');
+const app = express();
 const mongoose = require('mongoose');
+const session = require('express-session');
+const cookieParser = require('cookie-parser');
 const passport = require('passport');
 
+// Load User model
+require('./models/user');
 // passport config
 require('./config/passport')(passport);
-const app = express();
+
 // Load routes
 const auth = require('./routes/auth');
+
+// load keys
+const keys = require('./config/keys');
+
+// Map global promise
+mongoose.Promise = global.Promise;
+// connect to mongoose
+mongoose.connect(keys.mongoURI,
+   { useNewUrlParser: true })
+    .then(()=> console.log("MongoDB connected"))
+    .catch((err)=> console.log(err));
+
+
+// Cookie-parser
+app.use(cookieParser());
+app.use(session({
+  secret:'sagar.c',
+  resave:false,
+  saveUninitialized: false
+}));
+
+// Passport Middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// set global vars
+app.use((req,res,next)=>{
+  res.locals.user = req.user || null;
+  next();
+})
+
 
 app.get('/', (req,res) => {
   res.send('project started');
 })
 
 app.use('/auth', auth);
+
+
 
 
 const port = process.env.PORT || 3000
--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mongoURI:'mongodb+srv://storyBook:artist@story-book-vkvwc.mongodb.net/test?retryWrites=true&w=majority',
+  mongoURI:'mongodb+srv://storyBook:sagar@cluster0-eybxc.mongodb.net/test?retryWrites=true&w=majority',
   googleClientID:'516037692707-r52idcmus1f22aof77g6ovjvq9r6ic7k.apps.googleusercontent.com',
   googleClientSecret:'Rrr75u0Kvd8G7ydBbgnyOsvt'
 }
